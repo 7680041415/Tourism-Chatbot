a7ea5007c583803351150cac25e6c93fa665aa15
Ramya Sri <65210214+7680041415@users.noreply.github.com>
2024-09-26 14:29:19 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+
+HUGGINGFACEHUB_API_TOKEN = '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'
+
+PROJECT_ID = "fair-gradient-431814-p7"  # @param {type:"string"}
+REGION = "europe-west1"  # @param {type: "string"}
+INSTANCE = "tourism-mysql-db"  # @param {type: "string"}
+DATABASE = "Tourism_countries"  # @param {type: "string"}
+TABLE_NAME = "Vecor_table"  # @param {type: "string"}
+USER = 'root'
+PASSWORD = 'Likki1977'
+PUBLIC_IP_ADDRESS = "104.155.60.75"
+PORT = 3306
